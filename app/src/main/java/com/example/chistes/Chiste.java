86106430f667086677d87a95f9642d33d0429f64
robinson.tellez <robinson.tellez@telefonica.com>
2019-03-28 17:09:28 -0500

    Funcionando el Insert en FireBase

--- app/src/main/java/com/example/chistes/Chiste.java
+++ app/src/main/java/com/example/chistes/Chiste.java
@@ -1,4 +1,17 @@
 package com.example.chistes;
 
 public class Chiste {
+    private String Chiste;
+
+    public Chiste(){
+
+    }
+
+    public String getChiste() {
+        return Chiste;
+    }
+
+    public void setChiste(String chiste) {
+        this.Chiste = chiste;
+    }
 }
--- app/src/main/java/com/example/chistes/MainActivity.java
+++ app/src/main/java/com/example/chistes/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chistes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText txtchiste;
     Button btnchiste;
     DatabaseReference reff;
+    Chiste chiste;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "Conexion OK",Toast.LENGTH_LONG).show();
         txtchiste = (EditText) findViewById(R.id.txtChiste);
         btnchiste = (Button) findViewById(R.id.btnChiste);
-        reff = FirebaseDatabase.getInstance().getReference().child("Chiste");
+        chiste = new Chiste();
+        reff = FirebaseDatabase.getInstance().getReference().child("Chistes");
+        btnchiste.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                chiste.setChiste(txtchiste.getText().toString().trim());
+                reff.push().setValue(chiste);
+                Toast.makeText(MainActivity.this, "Chiste Agregado",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
